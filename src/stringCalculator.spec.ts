8f0d215aca9b9145d385e3dc5f6112ed9c0f5f79
sush.avazkhani <sush.avazkhani@cazoo.co.uk>
2021-02-16 09:31:01 +0000

    mob next [ci-skip]

--- src/stringCalculator.spec.ts
+++ src/stringCalculator.spec.ts
@@ -1,8 +1,15 @@
 import { expect } from "chai";
 
+const stringCalculator = (numbers: string) => {
+    return 0;
+}
+
 describe('stringCalculator', ()=>{
     it('should return 0 for an empty string',()=>{
         expect(stringCalculator("")).to.equal(0);
     })
 
+    it('should return 1 for an string "1"',()=>{
+        expect(stringCalculator("1")).to.equal(1);
+    })
 })
