87dca093ec028af4b97d5543b165c5df660e0c2e
Yemi Adebayo <yemi.adebayo@cazoo.co.uk>
2021-02-16 09:35:00 +0000

    mob next [ci-skip]

--- src/stringCalculator.spec.ts
+++ src/stringCalculator.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 
 const stringCalculator = (numbers: string) => {
+    if (numbers === '1') return 1;
     return 0;
 }
 
@@ -9,7 +10,11 @@ describe('stringCalculator', ()=>{
         expect(stringCalculator("")).to.equal(0);
     })
 
-    it('should return 1 for an string "1"',()=>{
+    it('should return 1 for a string "1"',()=>{
         expect(stringCalculator("1")).to.equal(1);
+    })
+
+    it('should return 2 for a string "2"',()=>{
+        expect(stringCalculator("2")).to.equal(2);
     })
 })
